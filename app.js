747e0dcedb4b5f21b0970375703784a9cb8670cb
Yahkob <jakobharclerode@gmail.com>
2014-07-27 23:01:49 -0700

    finished refactor

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var pubnub = PUBNUB.init({
-  publish_key   : 'pub-c-c428a047-ae49-4deb-b1fe-59e809d16826',
-  subscribe_key : 'sub-c-d3fc022c-06ec-11e4-a71e-02ee2ddab7fe'
+  publish_key   : '******************************************',
+  subscribe_key : '******************************************'
 });
 var outerWindow = this;
 var keyBinding = function(){
